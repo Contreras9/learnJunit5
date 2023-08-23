056d2549e4bc050175461e33d61c6fa83fa5e984
Hamzah Contreras <h.contreras9@outlook.com>
2023-08-23 16:55:09 -0500

    First Unit Test with JUnit

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -7,7 +7,11 @@ public class StringTest {
     @Test
     void test() {
         //Write test code to create an instance of a class in here
-        //Invoke the method
-        //Checks in place to verify that the output is correct => Assertions
+        //Invoke the method - square(4) => Code Under Test
+        //Checks in place to verify that the output is correct - (16) => Assertions
+
+        int actualLength = "ABCD".length();
+        int expectedLength = 4;
+        assertEquals(expectedLength,actualLength);
     }
 }
