e9868d3814b29655f09aa1a8789c3bc85467eff9
Hamzah Contreras <h.contreras9@outlook.com>
2023-08-23 17:09:01 -0500

    Writing JUnit Assertions

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StringTest {
 
     @Test
-    void test() {
+    void lengthBasic() {
         //Write test code to create an instance of a class in here
         //Invoke the method - square(4) => Code Under Test
         //Checks in place to verify that the output is correct - (16) => Assertions
@@ -13,5 +13,21 @@ public class StringTest {
         int actualLength = "ABCD".length();
         int expectedLength = 4;
         assertEquals(expectedLength,actualLength);
+    }
+
+    @Test
+    void toUpperCaseBasic() {
+        String str = "abcd";
+        String results = str.toUpperCase();
+        assertNotNull(results);
+        assertEquals("ABCD", results);
+    }
+
+    @Test
+    void containsBasic() {
+        String str = "abcdefgh";
+        boolean result = str.contains("ijk");
+        assertFalse(result, "true");
+        assertEquals(false, result);
     }
 }
