acd4be626718c3b9c41ad5a9805619b21932a31c
Hamzah Contreras <h.contreras9@outlook.com>
2023-08-23 15:33:04 -0500

    First Junit Test

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringTest {
+
+    @Test
+    void test() {
+        //Write test code to create an instance of a class in here
+        //Invoke the method
+        //Checks in place to verify that the output is correct => Assertions
+    }
+}
