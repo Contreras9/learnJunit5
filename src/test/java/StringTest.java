3d33e16df903c2aaa271224e1f0dd58e0d49f588
Hamzah Contreras <h.contreras9@outlook.com>
2023-08-23 19:12:58 -0500

    Setting up data for every test - @BeforeEach, @AfterEach

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,8 +1,21 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringTest {
+
+    @BeforeEach
+    void beforeEach(TestInfo info) {
+        System.out.println("Initialize Test Data for " + info.getDisplayName());
+    }
+
+    @AfterEach
+    void afterEach(TestInfo info) {
+        System.out.println("Clean up Test Data for " + info.getDisplayName());
+    }
 
     @Test
     void lengthBasic() {
